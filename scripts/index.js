7b0634805db7fd3748cbdad59800306f4a3008bd
sheils <schiu.nyc@gmail.com>
2024-01-09 19:49:54 -0500

    project4 finalstage

--- scripts/index.js
+++ scripts/index.js
@@ -36,3 +36,44 @@ buttonEdit.addEventListener("click", () => {
 buttonClose.addEventListener("click", () => {
   modal.classList.remove("modal_opened");
 });
+
+const input = modal.querySelectorAll(".form__input");
+const inputName = input[0];
+const inputDescription = input[1];
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const buttonSave = modal.querySelector(".form__button");
+const profileEditForm = modal.querySelector(".form");
+const cardTemplate = document.querySelector("#card-template").content;
+const cardList = document.querySelector(".cards__list");
+
+function profileInfoToForm() {
+  inputName.value = profileTitle.textContent;
+  inputDescription.value = profileSubtitle.textContent;
+}
+
+buttonEdit.addEventListener("click", profileInfoToForm);
+
+function updateProfile(evt) {
+  evt.preventDefault();
+
+  profileTitle.textContent = inputName.value;
+  profileSubtitle.textContent = inputDescription.value;
+}
+profileEditForm.addEventListener("submit", updateProfile);
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+
+  cardImage.setAttribute("src", data.link);
+  cardImage.setAttribute("alt", data.name);
+  cardTitle.textContent = data.name;
+
+  return cardElement;
+}
+initialCards.forEach((data) => {
+  const cardElement = getCardElement(data);
+  cardList.append(cardElement);
+});
